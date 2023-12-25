ac838d33e5ceca54ee398328178e4746c3037b7b
Jonatan <jonatansg@hotmail.com>
2023-12-25 21:24:11 +0000

    Modificación en el login para que muestre mensajes si los datos son incorrectos o los campos están vacíos.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    require_once("config/conexion.php");
+    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
+        require_once("models/Usuario.php");
+        $usuario = new Usuario();
+        $usuario->login();
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head lang="es">
@@ -19,32 +27,58 @@
     <link rel="stylesheet" href="public/css/main.css">
 </head>
 <body>
-
     <div class="page-center">
         <div class="page-center-in">
             <div class="container-fluid">
-                <form class="sign-box">
+                <form class="sign-box" action="" method="post" id="login_form">
                     <div class="sign-avatar">
                         <img src="public/img/avatar-sign.png" alt="">
                     </div>
                     <header class="sign-title">Acceso</header>
+                    
+                    <?php
+                        if (isset($_GET["m"])){
+                            switch($_GET["m"]){
+                                case "1";
+                                    ?>
+                                        <div class="alert alert-warning alert-icon alert-close alert-dismissible fade in" role="alert">
+							                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								                <span aria-hidden="true">×</span>
+							                </button>
+							                <i class="font-icon font-icon-warning"></i>
+							                El Usuario y/o Contraseña son incorrectos.
+						                </div>
+                                    <?php
+                                break;
+
+                                case "2";
+                                    ?>
+                                        <div class="alert alert-warning alert-icon alert-close alert-dismissible fade in" role="alert">
+							                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								                <span aria-hidden="true">×</span>
+							                </button>
+							                <i class="font-icon font-icon-warning"></i>
+							                Los campos están vacíos.
+						                </div>
+                                    <?php
+                                break;
+                            }
+                        }
+                    ?>
+
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="E-Mail or Phone"/>
+                        <input type="text" id="usu_correo" name="usu_correo" class="form-control" placeholder="E-Mail"/>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password"/>
+                        <input type="password" id="usu_pass" name="usu_pass" class="form-control" placeholder="Contraseña"/>
                     </div>
                     <div class="form-group">
-                        <div class="checkbox float-left">
-                            <input type="checkbox" id="signed-in"/>
-                            <label for="signed-in">Keep me signed in</label>
-                        </div>
                         <div class="float-right reset">
-                            <a href="reset-password.html">Reset Password</a>
+                            <a href="reset-password.html">Cambiar Contraseña</a>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-rounded">Sign in</button>
-                    <p class="sign-note">New to our website? <a href="sign-up.html">Sign up</a></p>
+                    <input type="hidden" name="enviar" class="form-control" value="si">
+                    <button type="submit" class="btn btn-rounded">Acceder</button>
                 </form>
             </div>
         </div>
