c70ce60b746f5c352aa89e06f5367bd33a12d91d
Jonatan <jonatansg@hotmail.com>
2023-12-27 16:32:41 +0000

    Creación vista Nuevo Ticket

--- view/NuevoTicket/index.php
+++ view/NuevoTicket/index.php
@@ -1,34 +1,87 @@
 <?php
-  require_once("../../config/conexion.php"); 
-  if(isset($_SESSION["usu_id"])){ 
+require_once("../../config/conexion.php");
+if (isset($_SESSION["usu_id"])) {
 ?>
-<!DOCTYPE html>
-<html>
-    <?php require_once("../MainHead/head.php");?>
-	<title>Gestión Tickets :: Nuevo Ticket</title>
-</head>
-<body class="with-side-menu">
+  <!DOCTYPE html>
+  <html>
+  <?php require_once("../MainHead/head.php"); ?>
+  <title>Gestión Tickets :: Nuevo Ticket</title>
+  </head>
+  <body class="with-side-menu">
 
-    <?php require_once("../MainHeader/header.php");?>
+    <?php require_once("../MainHeader/header.php"); ?>
 
-	<div class="mobile-menu-left-overlay"></div>
-    
-    <?php require_once("../MainNav/nav.php");?>
+    <div class="mobile-menu-left-overlay"></div>
 
-	<!-- Contenido -->
-	<div class="page-content">
-		<div class="container-fluid">
-			Blank page.
-		</div>
-	</div>
+    <?php require_once("../MainNav/nav.php"); ?>
+
+    <!-- Contenido -->
+    <div class="page-content">
+      <div class="container-fluid">
+
+        <header class="section-header">
+          <div class="tbl">
+            <div class="tbl-row">
+              <div class="tbl-cell">
+                <h3>Nuevo Ticket</h3>
+                <ol class="breadcrumb breadcrumb-simple">
+                  <li><a href="#">Home</a></li>
+                  <li class="active">Nuevo Ticket</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <div class="box-typical box-typical-padding">
+          <p>
+            Desde esta ventana podrá generar nuevos tickets:
+          </p>
+
+          <h5 class="m-t-lg with-border">Añadir Información</h5>
+
+          <div class="row">
+            <div class="col-lg-6">
+              <fieldset class="form-group">
+                <label class="form-label semibold" for="exampleInput">Categoría</label>
+                <select id="exampleSelect" class="form-control">
+                  <option>Hardware</option>
+                  <option>Software</option>
+                  <option>Otros</option>
+                </select>
+              </fieldset>
+            </div>
+            <div class="col-lg-6">
+              <fieldset class="form-group">
+                <label class="form-label semibold" for="exampleInputEmail1">Título</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Inserte Título">
+              </fieldset>
+            </div>
+            <div class="col-lg-12">
+              <fieldset class="form-group">
+                <label class="form-label semibold" for="exampleInputPassword1">Descripción</label>
+                <div class="summernote-theme-1">
+                  <textarea id="tick_descrip" class="summernote" name="name">Hello Summernote</textarea>
+                </div>
+              </fieldset>
+            </div>
+            <div class="col-lg-12">
+							<button type="submit" name="action" value="add" class="btn btn-rounded btn-inline btn-primary">Guardar</button>
+						</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
     <!-- Contenido -->
 
-    <?php require_once("../MainJs/js.php");?> 
-	
-	<script type="text/javascript" src="nuevoticket.js"></script>  
-                                 
-</body>
-</html>
+    <?php require_once("../MainJs/js.php"); ?>
+
+    <script type="text/javascript" src="nuevoticket.js"></script>
+
+  </body>
+
+  </html>
 <?php
   } else {
     header("Location:".Conectar::ruta()."index.php");
