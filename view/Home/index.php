40a3e060e3d8a04768f017e28c63f507149348e9
Jonatan <jonatansg@hotmail.com>
2024-01-03 17:43:38 +0000

    Añadida configuración para poder ver en el home los tickets dependiendo del usuario.

--- view/Home/index.php
+++ view/Home/index.php
@@ -1,36 +1,74 @@
 <?php
-  require_once("../../config/conexion.php"); 
-  if(isset($_SESSION["usu_id"])){ 
+require_once("../../config/conexion.php");
+if (isset($_SESSION["usu_id"])) {
 ?>
-<!DOCTYPE html>
-<html>
-    <?php require_once("../MainHead/head.php");?>
-	<title>Gestión Tickets :: Home</title>
-</head>
-<body class="with-side-menu">
+  <!DOCTYPE html>
+  <html>
+  <?php require_once("../MainHead/head.php"); ?>
+  <title>Gestión Tickets :: Home</title>
+  </head>
 
-    <?php require_once("../MainHeader/header.php");?>
+  <body class="with-side-menu">
 
-	<div class="mobile-menu-left-overlay"></div>
-    
-    <?php require_once("../MainNav/nav.php");?>
+    <?php require_once("../MainHeader/header.php"); ?>
 
-	<!-- Contenido -->
-	<div class="page-content">
-		<div class="container-fluid">
-			Blank page.
-		</div>
-	</div>
+    <div class="mobile-menu-left-overlay"></div>
+
+    <?php require_once("../MainNav/nav.php"); ?>
+
+    <!-- Contenido -->
+    <div class="page-content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xl-12">
+            <div class="row">
+              <div class="col-sm-4">
+                <article class="statistic-box green">
+                  <div>
+                    <div class="number" id="lbltotal"></div>
+                    <div class="caption">
+                      <div>Total de Tickets</div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="col-sm-4">
+                <article class="statistic-box yellow">
+                  <div>
+                    <div class="number" id="lbltotalabierto"></div>
+                    <div class="caption">
+                      <div>Total de Tickets Abiertos</div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="col-sm-4">
+                <article class="statistic-box red">
+                  <div>
+                    <div class="number" id="lbltotalcerrado"></div>
+                    <div class="caption">
+                      <div>Total de Tickets Cerrados</div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- Contenido -->
 
-    <?php require_once("../MainJs/js.php");?> 
-	
-	<script type="text/javascript" src="home.js"></script> 
-                                
-</body>
-</html>
+    <?php require_once("../MainJs/js.php"); ?>
+
+    <script type="text/javascript" src="home.js"></script>
+
+  </body>
+
+  </html>
 <?php
-  } else {
-    header("Location:".Conectar::ruta()."index.php");
-  }
+} else {
+  header("Location:" . Conectar::ruta() . "index.php");
+}
 ?>
