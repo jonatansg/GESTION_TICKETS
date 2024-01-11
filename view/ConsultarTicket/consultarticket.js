d87c2cbd03c2b447b76aef337c3e70ef165a3588
Jonatan <jonatansg@hotmail.com>
2024-01-11 22:55:27 +0000

    Reabrir ticket con rol de usuario o soporte.

--- view/ConsultarTicket/consultarticket.js
+++ view/ConsultarTicket/consultarticket.js
@@ -162,4 +162,33 @@ function guardar(e){
     });
 }
 
+function CambiarEstado(tick_id){
+    swal({
+        title: "¡Advertencia!",
+        text: "¿Está seguro de Reabrir el Ticket?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-warning",
+        confirmButtonText: "Sí",
+        cancelButtonText: "No",
+        closeOnConfirm: false
+    },
+    function(isConfirm) {
+        if (isConfirm) {
+            $.post("../../controller/ticket.php?op=reabrir", {tick_id : tick_id,usu_id : usu_id}, function (data) {
+
+            });
+
+            $('#ticket_data').DataTable().ajax.reload();	
+
+            swal({
+                title: "¡Confirmación!",
+                text: "Ticket Abierto.",
+                type: "success",
+                confirmButtonClass: "btn-success"
+            });
+        }
+    });
+}
+
 init();
